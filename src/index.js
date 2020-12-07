758fe0940be7832a0d6f4718b62dd13fe656bf9c
gustavoGarciaPereira <gusgurtavo@gmail.com>
2020-12-07 19:24:21 -0300

    regatorando

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const axios = require('axios');
- 
-//const fetch = require('node-fetch');
-//var request = require('request-promise');
+
 //require('dotenv').config()
 
 const app = express();
 app.get('/', async(req,res)=>{
-    console.log("gustavo get")
-    res.status(200).json(await getReceitas())
+    res.status(200).json(await formatReceitas())
 })
 
-// Want to use async/await? Add the `async` keyword to your outer function/method.
+/**
+ {
+    title: 'Garlicky Mushroom Masala Omelet',
+    href: 'http://www.recipezaar.com/Garlicky-Mushroom-Masala-Omelet-306233',
+    ingredients: 'tomato, cilantro, eggs, ginger, garlic, green chilies, mushroom, mustard seed, black pepper, salt, green onion, vegetable oil',
+    thumbnail: 'http://img.recipepuppy.com/215908.jpg'
+  },
+
+ */
+
+async function formatReceitas() {
+    try {
+        const receitas = await getReceitas()
+
+        const receitas_form = receitas.map((x)=>{
+            return{
+                "titulo":x.title,
+                "link":x.href,
+                "ingredientes":x.ingredients,
+                "thumbnail":x.thumbnail
+            }
+        })
+      return receitas_form
+    } catch (error) {
+      console.error(error);
+    }
+}
+
 async function getReceitas() {
     try {
       const response = await axios.get('http://www.recipepuppy.com/api/?i=tomato,onion&q=omelet&p=3');
@@ -19,7 +43,7 @@ async function getReceitas() {
     } catch (error) {
       console.error(error);
     }
-  }
+}
 
 app.listen('3001',()=>{
     console.log("ouvindo")
